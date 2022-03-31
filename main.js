de0ca9ebc772cdad26d5b433b2e64dc1f071617d
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-31 08:56:44 +0100

    Immagine

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#root',
     data: {
-      message: 'La vita è bella!'
+      message: 'La vita è sempre più bella!',
+      image: 'https://source.unsplash.com/random'
     }
   });
